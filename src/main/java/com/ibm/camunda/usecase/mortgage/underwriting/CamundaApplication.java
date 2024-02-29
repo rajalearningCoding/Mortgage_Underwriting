8beecc3f430120f63c325353719a1f5b15b99974
Raja <mdrajanit2008@gmail.com>
2024-02-29 21:36:52 +0530

    Initial commit

---
+++ src/main/java/com/ibm/camunda/usecase/mortgage/underwriting/CamundaApplication.java
@@ -0,0 +1,15 @@
+package com.ibm.camunda.usecase.mortgage.underwriting;
+
+import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableProcessApplication("Mortgage_Underwriting")
+public class CamundaApplication {
+
+  public static void main(String... args) {
+    SpringApplication.run(CamundaApplication.class, args);
+  }
+
+}
---
+++ src/main/java/com/ibm/camunda/usecase/mortgage/underwriting/ProcessConstants.java
@@ -0,0 +1,7 @@
+package com.ibm.camunda.usecase.mortgage.underwriting;
+
+public class ProcessConstants {
+
+  public static final String PROCESS_DEFINITION_KEY = "Mortgage_Underwriting"; // BPMN Process ID
+
+}
