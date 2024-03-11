7c309edf877083036339bd436168d40a4e46e521
PriyotoshRoy <PriyotoshRoy@IBM-PF20DR6D.in.ibm.com>
2024-03-11 14:08:37 +0530

    Added Cancel Request first stage for Signal and Message.

---
+++ src/main/java/com/ibm/camunda/usecase/mortgage/underwriting/delegate/CancelRequest.java
@@ -0,0 +1,60 @@
+package com.ibm.camunda.usecase.mortgage.underwriting.delegate;
+
+
+
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.stereotype.Component;
+
+
+import com.ibm.camunda.usecase.mortgage.underwriting.model.Borrower;
+import com.ibm.camunda.usecase.mortgage.underwriting.repository.BorrowerRepository;
+
+@Component("cancelRequest")
+public class CancelRequest implements JavaDelegate {
+
+	
+	 @Autowired
+	    private BorrowerRepository borrowerRepository;
+	
+	
+	@Override
+	public void execute(DelegateExecution execution) throws Exception {
+		// TODO Auto-generated method stub
+		
+		
+		    String caseStatus=(String) execution.getVariable("caseStatus");
+		
+
+	       	System.out.println("Cancel Request");
+		    String panNo = (String) execution.getVariable("panNo");
+		    System.out.println(panNo);//Printing panNo Value
+		    Borrower panNo1 = this.borrowerRepository.findByPanNo(panNo);
+
+		        if(panNo1!=null)
+		        {
+		        	panNo1.setCaseStatus(caseStatus);
+		        	this.borrowerRepository.save(panNo1);
+		        	
+		        	System.out.println("Request has been cancelled ");
+					
+		        }
+		        else 
+		        {	        	
+		        	  System.out.println("Request Cancellation Not Request Got Cancelled");
+		        }
+	       
+
+		
+		
+		
+		}	
+		
+		
+}
+
+
+
